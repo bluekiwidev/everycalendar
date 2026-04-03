d1c2a19757bd9f6a629175a5ee99d7599e05ab9e
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-04-03 16:55:36 +1300

    Update font styles and restructure HTML layout for improved design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,18 @@
-<div class="container">
+<div class="topnav">
 	<h1>The every calendar</h1>
-	<p>
-		This is a calendar where anyone can dedicate a day to somthing. It can be anything, from a holiday to a birthday, or even just a random day that you want to remember. The only rule is that it has to be something that you care about. So go ahead and dedicate a day to something that matters to you!
-	</p>
-	<div
-		class="cf-turnstile"
-		data-sitekey="0x4AAAAAACzw-393AjCmz36n"
-		data-theme="dark"
-		data-size="normal"
-		data-callback="onSuccess"
-	></div>
 </div>
+
+<p>
+	This is a calendar where anyone can dedicate a day to somthing. It can be anything, from a holiday
+	to a birthday, or even just a random day that you want to remember. The only rule is that it has
+	to be something that you care about. So go ahead and dedicate a day to something that matters to
+	you!
+</p>
+
+<div
+	class="cf-turnstile"
+	data-sitekey="0x4AAAAAACzw-393AjCmz36n"
+	data-theme="dark"
+	data-size="normal"
+	data-callback="onSuccess"
+></div>
