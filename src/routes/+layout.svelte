da9b5ade8f4802e4b4b7a8ef540a97f0efc31258
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-04-03 14:36:30 +1300

    Added Cloudflare Turnstile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>every calendar</title>
 </svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container">
+	<h1>Welcome to the every calendar</h1>
+	<p>
+		This is a calendar that shows every day of the year. You can click on any day to see more
+		details about it.
+	</p>
+	<div
+		class="cf-turnstile"
+		data-sitekey="0x4AAAAAACzw-393AjCmz36n"
+		data-theme="dark"
+		data-size="normal"
+		data-callback="onSuccess"
+	></div>
+</div>
