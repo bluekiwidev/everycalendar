610e7f48c3216636db86a05f0eaa08178b7f9729
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-04-05 13:52:43 +1200

    feat: update month display and remove unused styles

--- src/routes/[month]/+page.svelte
+++ src/routes/[month]/+page.svelte
@@ -1,7 +1,43 @@
 <script>
   import { page } from '$app/state';
+  const months = [
+		'1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'potato',
+    'cheese',
+    'ketchup',
+    'mustard',
+    'mayonnaise',
+    'coke',
+    'pepsi',
+    'sprite',
+    'fanta',
+
+	];
 </script>
 
-<h1>
-  Welcome to the {page.params.month} month of the year
-</h1>
+<!-- remember page.params.month is the month in the url -->
+<div class="topnav">
+	<h1>The everyone calendar</h1>
+</div>
+
+<h2>{page.params.month}</h2>
+
+<div class="months">
+	{#each months as month, index}
+		<a href={"/" + month.toLocaleLowerCase()} class="month">
+			<h2 class="month-text">{month}</h2>
+			<div class="month-stats">
+				<i class="fa-solid fa-circle-dot"></i>
+				<h6>0 events dedicated</h6>
+			</div>
+		</a>
+	{/each}
+</div>
